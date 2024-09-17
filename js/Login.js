dff1f1af247435731eb79882954cdc9b275b5bd2
NICOKTGIRI <nikolukas1234567@gmail.com>
2024-09-16 20:55:11 -0500

    ...

--- js/Login.js
+++ js/Login.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // Simulando un inicio de sesión exitoso para propósitos de ejemplo
         if (email === "fernanda@example.com" && password === "123456") {
             // Redirigir al usuario a Usuario.html después de iniciar sesión
-            window.location.href = "../html/inicio.html";
+            window.location.href = "/html/inicio.html";
         } else {
             // Mostrar un mensaje de error o realizar otras acciones si el inicio de sesión es incorrecto
             alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
